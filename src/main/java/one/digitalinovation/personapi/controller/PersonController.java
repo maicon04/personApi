9ce464a32001f47e14dc60444a0803fca28b19d9
maicon04 <maikonmarkes11@hotmail.com>
2021-10-14 23:19:40 -0300

    Change for testing Heroku

--- src/main/java/one/digitalinovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinovation/personapi/controller/PersonController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
     @GetMapping
     public String getBook(){
-        return "Api Test!";
+        return "Testando funcionamento no HeroKu!";
     }
 }
