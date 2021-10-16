f3b99f75162fdad2b1a5fe77789b4fd7b3704214
maicon04 <maikonmarkes11@hotmail.com>
2021-10-15 22:48:29 -0300

    Create Service Person

---
+++ src/main/java/one/digitalinovation/personapi/service/PersonService.java
@@ -0,0 +1,25 @@
+package one.digitalinovation.personapi.service;
+
+import one.digitalinovation.personapi.dto.MessageResponseDTO;
+import one.digitalinovation.personapi.entity.Person;
+import one.digitalinovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
