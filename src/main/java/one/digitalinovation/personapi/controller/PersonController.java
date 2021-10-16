24b668cbcf0e7048b483ae47b2367dd66bb43533
maicon04 <maikonmarkes11@hotmail.com>
2021-10-16 00:17:20 -0300

    Development ListAll and CreatePerson

--- src/main/java/one/digitalinovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinovation/personapi/controller/PersonController.java
@@ -1,15 +1,17 @@
 package one.digitalinovation.personapi.controller;
 
+import one.digitalinovation.personapi.dto.request.PersonDTO;
 import one.digitalinovation.personapi.dto.response.MessageResponseDTO;
-import one.digitalinovation.personapi.entity.Person;
 import one.digitalinovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/people")
-
 public class PersonController {
 
     private PersonService personService;
@@ -21,7 +23,12 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        return personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+       return personService.listAll();
     }
 }
--- src/main/java/one/digitalinovation/personapi/dto/request/PersonDTO.java
+++ src/main/java/one/digitalinovation/personapi/dto/request/PersonDTO.java
@@ -4,9 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
 import org.hibernate.validator.constraints.br.CPF;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
